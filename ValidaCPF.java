af0ae0c4cd3e647c75fe1cad52c46c9234bbcde4
Samuel Brayan <85649169+samuelnascimento2021@users.noreply.github.com>
2022-03-28 21:36:32 -0300

    Update ValidaCPF.java

--- ValidaCPF.java
+++ ValidaCPF.java
@@ -2,7 +2,7 @@ package Lista3;
 import java.util.InputMismatchException;
 public class ValidaCPF {
 	public static boolean isCPF(String CPF) {
-        // considera-se erro CPF's formados por uma sequencia de numeros iguais
+        
         if (CPF.equals("00000000000") ||
             CPF.equals("11111111111") ||
             CPF.equals("22222222222") || CPF.equals("33333333333") ||
@@ -15,15 +15,13 @@ public class ValidaCPF {
         char dig10, dig11;
         int sm, i, r, num, peso;
 
-        // "try" - protege o codigo para eventuais erros de conversao de tipo (int)
+        
         try {
-        // Calculo do 1o. Digito Verificador
+        
             sm = 0;
             peso = 10;
             for (i=0; i<9; i++) {
-        // converte o i-esimo caractere do CPF em um numero:
-        // por exemplo, transforma o caractere '0' no inteiro 0
-        // (48 eh a posicao de '0' na tabela ASCII)
+      
             num = (int)(CPF.charAt(i) - 48);
             sm = sm + (num * peso);
             peso = peso - 1;
@@ -32,9 +30,9 @@ public class ValidaCPF {
             r = 11 - (sm % 11);
             if ((r == 10) || (r == 11))
                 dig10 = '0';
-            else dig10 = (char)(r + 48); // converte no respectivo caractere numerico
+            else dig10 = (char)(r + 48); 
 
-        // Calculo do 2o. Digito Verificador
+       
             sm = 0;
             peso = 11;
             for(i=0; i<10; i++) {
@@ -48,7 +46,7 @@ public class ValidaCPF {
                  dig11 = '0';
             else dig11 = (char)(r + 48);
 
-        // Verifica se os digitos calculados conferem com os digitos informados.
+      
             if ((dig10 == CPF.charAt(9)) && (dig11 == CPF.charAt(10)))
                  return(true);
             else return(false);
